02ea83958a3eea2303a1f48ced5e0e85319014f6
Abetov_Khusan <daler.saidov@aliftech.uz>
2023-01-20 08:52:02 +0500

    еуые

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const btn = document.querySelector("button");
+const loginBtn = document.querySelector(".login-form button");
+const createContactBtn = document.querySelector(".create-contact-form button");
 
-console.log("test");
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
 
-btn.addEventListener("click", () => {
+loginBtn.addEventListener("click", () => {
   const name = document.querySelector(".name");
   const password = document.querySelector(".password");
 
@@ -21,6 +26,30 @@ btn.addEventListener("click", () => {
   getLoginAccess(obj);
 });
 
+createContactBtn.addEventListener("click", () => {
+  const name = document.querySelector(".create-contact-form .name");
+  const phone = document.querySelector(".create-contact-form .phone");
+
+  if (!name.value) {
+    alert("Please enter name");
+    return;
+  } else if (!phone.value) {
+    alert("Please enter phone");
+    return;
+  }
+
+  let obj = {
+    name: name.value,
+    phone: phone.value,
+  };
+  if (getCookie("userId")) {
+    alert("Not authenticated");
+    return;
+  }
+  createContact(obj.name, obj.phone);
+  alert("contact created");
+});
+
 async function getLoginAccess(obj) {
   let response = await fetch("/login", {
     method: "POST",
@@ -31,12 +60,12 @@ async function getLoginAccess(obj) {
   if (response.status === 401) {
     alert("error");
   } else {
-    // renderContacts();
+    renderContacts();
   }
 }
 
 async function createContact(name, phone) {
-  let response = await fetch("/contacts", {
+  await fetch("/contacts", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, phone }),
@@ -47,27 +76,26 @@ async function renderContacts() {
   let response = await fetch("/contacts", {
     method: "GET",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(query),
   });
-  let json = await response.json();
+  let { contacts } = await response.json();
 
-  const contacts = json.contacts;
-  const ul = document.querySelector("ul");
-  ul.innerHTML = null;
+  // const contacts = json.contacts;
+  // const ul = document.querySelector("ul");
+  // ul.innerHTML = null;
 
-  for (let el of contacts) {
-    const li = document.createElement("li");
-    // li.innerHTML = `<h3>${el.title}</h3>
-    //       <h4>${el.price}</h4>`;
+  // for (let el of contacts) {
+  //   const li = document.createElement("li");
+  //   // li.innerHTML = `<h3>${el.title}</h3>
+  //   //       <h4>${el.price}</h4>`;
 
-    const h3 = document.createElement("h3");
-    const h4 = document.createElement("h4");
+  //   const h3 = document.createElement("h3");
+  //   const h4 = document.createElement("h4");
 
-    h3.textContent = el.title;
-    h4.textContent = el.price;
+  //   h3.textContent = el.title;
+  //   h4.textContent = el.price;
 
-    li.append(h3, h4);
+  //   li.append(h3, h4);
 
-    ul.append(li);
-  }
+  //   ul.append(li);
+  // }
 }
